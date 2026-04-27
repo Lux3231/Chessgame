cbe854e97cb3b734575831195881b2ba83741d6d
Student <foobar@sr.htlweiz.at>
2026-04-27 11:09:02 +0200

    Chess Board can be printed out correctly

--- GameLib/Class1.cs
+++ GameLib/Class1.cs
@@ -1,6 +1,34 @@
 ﻿namespace GameLib;
 
-public class Class1
+public class Board
 {
+    private string BoardLine = "+---+---+---+---+---+---+---+---+";
+    private string WhiteField = "| # ";
+    private string BlackField = "|   ";
+
+
+    public override string ToString()
+    {
+        string result = "";
+
+        for (int row = 0; row < 8; row++)
+        {
+            result += BoardLine + "\n";
+
+            for (int col = 0; col < 8; col++)
+            {
+                if((row + col) % 2 == 0){
+                    result += WhiteField;
+                }
+                else{
+                    result += BlackField;
+                }
+            }
+            result += "|\n";
+        }
+        result += BoardLine;
+
+        return result;
+    }
 
 }
--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GameLib;
+
+namespace Prgram;
+
+public class Program {
+
+    public static int Main(){
+        Board Board = new();
+
+        Console.WriteLine(Board.ToString());
+        return 0;
+    }
+}
